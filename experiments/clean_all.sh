5c0f82f597259549f8eac243947a5c4b449b0031
adamzr2000 <adamzr2000@gmail.com>
2024-02-15 12:12:43 +0000

    working

--- experiments/clean_all.sh
+++ experiments/clean_all.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Delete all CSV files in consumers/ and providers/ directories
-find consumers/ providers/ -type f -name "*.csv" -exec rm -f {} \;
+# Delete all CSV files in consumer/ and provider/ directories
+find consumer/ provider/ -type f -name "*.csv" -exec rm -f {} \;
 
 echo "All CSV files in consumers/ and providers/ have been deleted."
+
