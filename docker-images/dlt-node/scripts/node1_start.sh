44a1c3825fe55181d07b3a3f37f8893cc70209ad
adamzr2000 <adamzr2000@gmail.com>
2024-02-15 15:17:12 +0000

    monitor

--- docker-images/dlt-node/scripts/node1_start.sh
+++ docker-images/dlt-node/scripts/node1_start.sh
@@ -8,7 +8,7 @@ echo "$output"
 source .env
 
 # Define the command
-command="geth --identity "node1" --syncmode "full" --ws --ws.addr $IP_NODE_1  --ws.port $WS_PORT_NODE_1 --datadir node1 --port $ETH_PORT_NODE_1 --bootnodes $BOOTNODE_URL --ws.api "eth,net,web3,personal,miner,admin,clique" --networkid 1234 --nat "any" --allow-insecure-unlock --authrpc.port $RPC_PORT_NODE_1 --ipcdisable --unlock $ETHERBASE_NODE_1 --password password.txt --mine --snapshot=false --miner.etherbase $ETHERBASE_NODE_1" 
+command="geth --identity "node1" --syncmode "full" --ws --ws.addr $IP_NODE_1  --ws.port $WS_PORT_NODE_1 --datadir node1 --port $ETH_PORT_NODE_1 --bootnodes $BOOTNODE_URL --ws.api "eth,net,web3,personal,miner,admin,clique" --networkid 1234 --nat "any" --allow-insecure-unlock --authrpc.port $RPC_PORT_NODE_1 --ipcdisable --unlock $ETHERBASE_NODE_1 --password password.txt --mine --snapshot=false --miner.etherbase $ETHERBASE_NODE_1 --ethstats node1:$WS_SECRET@$ETH_NETSATS_IP:$ETH_NETSATS_PORT" 
 
 # Execute the command
 eval $command
